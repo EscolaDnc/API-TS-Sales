526b875efeacad6aeb296f2146150df248acac57
codethi <docencia.thiago@gmail.com>
2024-07-02 23:08:15 -0400

    feat: entities

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,31 +1,43 @@
-import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import { AppDataSource } from '@shared/typeorm/data-source';
 
-const app = express();
+AppDataSource.initialize()
+  .then(async () => {
+    const app = express();
+    app.use(cors());
+    app.use(express.json());
 
-app.use(cors());
-app.use(express.json());
+    app.use(routes);
+    app.use(
+      (
+        error: Error,
+        request: Request,
+        response: Response,
+        next: NextFunction,
+      ) => {
+        if (error instanceof AppError) {
+          return response.status(error.statusCode).json({
+            status: 'error',
+            message: error.message,
+          });
+        }
 
-app.use(routes);
-app.use(
-  (error: Error, request: Request, response: Response, next: NextFunction) => {
-    if (error instanceof AppError) {
-      return response.status(error.statusCode).json({
-        status: 'error',
-        message: error.message,
-      });
-    }
+        return response.status(500).json({
+          status: 'error',
+          message: 'Internal server error',
+        });
+      },
+    );
 
-    return response.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
+    console.log('Connected to the database! 🎉');
+
+    return app.listen(3333, () => {
+      console.log('Server started on port 3333! 🏆');
     });
-  },
-);
-
-app.listen(3333, () => {
-  console.log('Server started on port 3333! 🏆');
-});
+  })
+  .catch(error => {
+    console.error('Failed to connect to the database:', error);
+  });
