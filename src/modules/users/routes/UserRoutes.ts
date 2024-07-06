8bc96c23b8522bb67def4d700db06c61a4af7264
codethi <docencia.thiago@gmail.com>
2024-07-06 00:25:44 -0400

    feat: protected routes

--- src/modules/users/routes/UserRoutes.ts
+++ src/modules/users/routes/UserRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import UsersController from '../controllers/UserController';
 import { userSchemaValidation } from '../schemas/UserSchema';
+import isAuthenticated from '@shared/http/middlewares/isAuthenticated';
 
 const usersRouter = Router();
 const usersController = new UsersController();
 
-usersRouter.get('/', usersController.index);
+usersRouter.get('/', isAuthenticated, usersController.index);
 usersRouter.post('/', userSchemaValidation, usersController.create);
 
 export default usersRouter;
