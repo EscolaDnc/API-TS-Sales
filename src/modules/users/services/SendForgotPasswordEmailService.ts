d3e35692950f627f84f351fa92914ebe99f42146
codethi <docencia.thiago@gmail.com>
2024-07-10 11:46:39 -0400

    feat: alter html email

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -21,12 +21,12 @@ class SendForgotPasswordEmailService {
       to: email,
       subject: 'My Sales Recovery Password',
       body: `
-        <div style="font-family: Arial, sans-serif; padding: 20px; color: #333; text-align: center; border: 2px solid #041d40; border-radius: 10px; background-color: #F0F8FF; margin: auto; width: 60%;">
+        <div style="font-family: Arial, sans-serif; padding: 20px; color: #333; text-align: center; border: 2px solid #041d40; border-radius: 10px; margin: auto; width: 60%;">
           <h1 style="color: #041d40;">Password Reset Verification Code</h1>
           <h3 style="color: #041d40;">Dear ${user.name},</h3>
           <p style="font-size: 16px; color: #333;">Recover your password with this token:</p>
           <p>
-            <strong style="border: 2px dashed #041d40; background-color: #E6F7FF; padding: 10px; border-radius: 5px; font-size: 16px; color: #4A90E2;">${token?.token}</strong>
+            <strong style="border: 2px dashed #041d40; padding: 10px; border-radius: 5px; font-size: 16px; color: #041d40;">${token?.token}</strong>
           </p>
           <p style="margin-top: 20px;">Best regards,<br><span style="font-weight: bold; color: #041d40;">My Sales Staff</span></p>
         </div>
