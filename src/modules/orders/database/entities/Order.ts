a79f245bfe5c231a7f48b7161e738345005b6aec
codethi <docencia.thiago@gmail.com>
2024-07-11 22:33:57 -0300

    feat: entity order

--- src/modules/orders/database/entities/Order.ts
+++ src/modules/orders/database/entities/Order.ts
@@ -4,26 +4,18 @@ import {
   Entity,
   JoinColumn,
   ManyToOne,
-  OneToMany,
   PrimaryGeneratedColumn,
   UpdateDateColumn,
 } from 'typeorm';
-import OrdersProducts from './OrdersProducts';
-
 
 @Entity('orders')
 class Order {
-  @PrimaryGeneratedColumn()
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ManyToOne(() => Customer)
+  @ManyToOne(() => Customer )
   @JoinColumn({ name: 'customer_id' })
   customer: Customer;
-
-  @OneToMany(() => OrdersProducts, order_products => order_products.order, {
-    cascade: true,
-  })
-  order_products: OrdersProducts[];
 
   @CreateDateColumn()
   created_at: Date;
