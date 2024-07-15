756f6eacbea7333aa435ff7d1592eb5487b96f6f
codethi <docencia.thiago@gmail.com>
2024-07-15 12:10:38 -0300

    feat: cross-env

--- src/shared/infra/typeorm/migrations/1720576030117-CreateUsersToken.ts
+++ src/shared/infra/typeorm/migrations/1720576030117-CreateUsersToken.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUsersToken1720576030117 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`);
     await queryRunner.createTable(
       new Table({
         name: 'user_tokens',
